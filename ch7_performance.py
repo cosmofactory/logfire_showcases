a1a88b182842e655025b3b5a5135a0e2e9d71780
cosmofactory <nikssor@yandex.ru>
2024-11-24 13:24:33 +0100

    add docstring

--- ch7_performance.py
+++ ch7_performance.py
@@ -6,15 +6,20 @@ import logfire
 
 @logfire.instrument()
 def decorated_func(sleep_time: float) -> None:
+    """
+    This function has a logging module attached and sleeps for given time.
+    """
     sleep(sleep_time)
     logfire.info("Sleeping for {sleep_time} seconds", sleep_time=sleep_time)
 
 
 def clean_func(sleep_time: float) -> None:
+    """This function sleeps for given time."""
     sleep(sleep_time)
 
 
 def main(func: Callable, sleep_time: float) -> None:
+    """Run functions and measure execution time."""
     start = time()
     for _ in range(20):
         func(sleep_time)
